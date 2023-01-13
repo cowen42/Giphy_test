4c11498e9e204799318a04c126954213677a65ba
Carl Owen <carl.owen@hmhco.com>
2023-01-13 18:32:03 +0000

    Revert "tidy"
    This reverts commit c1205f535a7db06917dcd60f4bb92d58b59ae415.

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -6,10 +6,10 @@
 
 /* eslint-disable no-console */
 
-import jsf from "json-schema-faker";
-import { schema } from "./mockDataSchema";
-import fs from "fs";
-import chalk from "chalk";
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
 
 const json = JSON.stringify(jsf(schema));
 
--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,17 +1,15 @@
-import express from "express";
-import webpack from "webpack";
-import config from "../webpack.config.dev";
-import start from "../api/startApp";
+import express from 'express';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+import start from '../api/startApp';
 
 const port = 3030;
 const app = express();
 const compiler = webpack(config);
 
-app.use(
-  require("webpack-dev-middleware")(compiler, {
+app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
-    publicPath: config.output.publicPath,
-  })
-);
+    publicPath: config.output.publicPath
+}));
 
 start(app, port);
