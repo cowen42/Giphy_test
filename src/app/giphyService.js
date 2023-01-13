c1205f535a7db06917dcd60f4bb92d58b59ae415
Carl Owen <carl.owen@hmhco.com>
2023-01-13 18:24:19 +0000

    tidy

--- src/app/giphyService.js
+++ src/app/giphyService.js
@@ -1,43 +1,38 @@
 /*eslint-disable no-unused-vars */
 /*eslint-disable no-console */
-const giphyService = function($http) {
+const giphyService = function ($http) {
+  let returnVal = {};
 
-    let returnVal = {};
+  returnVal.searchApi = function (searchTerm, offset) {
+    if (!offset) offset = 0;
+    return $http.get(
+      `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=1PxfCFVSyCqRKaRLa6G02bZWHcDbobVv&limit=15&offset=${offset}`
+    );
+  };
 
-    returnVal.searchApi = function(searchTerm, offset) {
+  returnVal.addToFavourites = function (data) {
+    $http.post("/api/favourites", data).then(
+      function (success) {},
+      function (fail) {
+        console.log(fail);
+      }
+    );
+  };
 
-        if (!offset) offset = 0;
-        return $http.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=1PxfCFVSyCqRKaRLa6G02bZWHcDbobVv&limit=15&offset=${offset}`);
+  returnVal.removeFromFavourites = function (data) {
+    $http.delete(`/api/favourites?id=${data.id}`).then(
+      function (success) {},
+      function (fail) {
+        console.log(fail);
+      }
+    );
+  };
 
-    };
+  returnVal.getFavourites = function () {
+    return $http.get("/api/favourites");
+  };
 
-    returnVal.addToFavourites = function(data) {
-
-        $http.post('/api/favourites', data).then(
-            function(success) {},
-            function(fail) {
-                console.log(fail);
-            });
-    };
-
-    returnVal.removeFromFavourites = function(data) {
-
-        $http.delete(`/api/favourites?id=${data.id}`).then(
-            function(success) {},
-            function(fail) {
-                console.log(fail);
-            });
-    };
-
-
-
-
-    returnVal.getFavourites = function() {
-
-        return $http.get('/api/favourites');
-    };
-
-    return returnVal;
+  return returnVal;
 };
 
 export default giphyService;
