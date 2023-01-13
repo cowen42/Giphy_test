cb3105e57275f84baaa3689114f60002fd06cf5f
Carl Owen <carl.owen@hmhco.com>
2023-01-13 18:33:23 +0000

    Revert "tidy"
    This reverts commit 68fd1e5ba5c4694684b1f0bb3051ea592dbc5679.

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,11 +1,11 @@
-import express from "express";
-import compression from "compression";
-import start from "../api/startApp";
+import express from 'express';
+import compression from 'compression';
+import start from '../api/startApp'
 
 const port = 3040;
 const app = express();
 
 app.use(compression());
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
 start(app, port);
--- src/app/mainController.js
+++ src/app/mainController.js
@@ -1,80 +1,80 @@
 /*eslint-disable no-unused-vars */
 /*eslint-disable no-console */
 
-import _ from "lodash";
+import _ from 'lodash';
 
-export default function (giphyService) {
-  const main = this;
-  let lastCallToApi = {};
-  main.offset = 0;
-  main.results = [];
+export default function(giphyService) {
 
-  function mapResults(results) {
-    results.then(
-      function (results) {
-        let mappedResults = _.map(results.data.data, (item) => {
-          return {
-            id: item.id,
-            thumbnail: item.images.fixed_width_still.url,
-            fullSize: item.images.original.url,
-            source: item.source_post_url,
-          };
-        });
-        main.results = mappedResults;
-      },
-      function (fail) {
-        console.log(fail);
-      }
-    );
-  }
+    const main = this;
+    let lastCallToApi = {};
+    main.offset = 0;
+    main.results = [];
 
-  main.getKittens = function (offset) {
-    let results = giphyService.searchApi("kittens", offset);
-    if (lastCallToApi !== main.getKittens) main.offset = 0;
-    lastCallToApi = main.getKittens;
-    mapResults(results);
-  };
+    function mapResults(results) {
 
-  main.getPuppies = function (offset) {
-    let results = giphyService.searchApi("puppies", offset);
-    if (lastCallToApi !== main.getPuppies) main.offset = 0;
-    lastCallToApi = main.getPuppies;
-    mapResults(results);
-  };
+        results.then(
+            function(results) {
+                let mappedResults = _.map(results.data.data, item => {
+                    return {
+                        id: item.id,
+                        thumbnail: item.images.fixed_width_still.url,
+                        fullSize: item.images.original.url,
+                        source: item.source_post_url
+                    }
+                });
+                main.results = mappedResults;
+            },
+            function(fail) {
+                console.log(fail);
+            });
+    }
 
-  main.more = function () {
-    main.offset += 15;
-    lastCallToApi(main.offset);
-  };
+    main.getKittens = function(offset) {
+        let results = giphyService.searchApi('kittens', offset);
+        if (lastCallToApi !== main.getKittens) main.offset = 0
+        lastCallToApi = main.getKittens;
+        mapResults(results);
+    }
 
-  main.back = function () {
-    main.offset -= 15;
-    if (main.offset <= 0) main.offset = 0;
-    lastCallToApi(main.offset);
-  };
+    main.getPuppies = function(offset) {
+        let results = giphyService.searchApi('puppies', offset);
+        if (lastCallToApi !== main.getPuppies) main.offset = 0
+        lastCallToApi = main.getPuppies;
+        mapResults(results);
+    }
 
-  main.displayPopup = function (item) {
-    main.selectedItem = item;
-  };
+    main.more = function() {
+        main.offset += 15;
+        lastCallToApi(main.offset);
+    }
 
-  main.addToFavourites = function (item) {
-    giphyService.addToFavourites(item);
-  };
+    main.back = function() {
+        main.offset -= 15;
+        if (main.offset <= 0) main.offset = 0;
+        lastCallToApi(main.offset);
+    }
 
-  main.removeFromFavourites = function (item) {
-    giphyService.removeFromFavourites(item);
-  };
+    main.displayPopup = function(item) {
+        main.selectedItem = item;
+    }
 
-  main.getFavourites = function () {
-    let results = giphyService.getFavourites().then(
-      function (results) {
-        main.favourites = results.data.favourites;
-      },
-      function (fail) {
-        console.log(fail);
-      }
-    );
-  };
+    main.addToFavourites = function(item) {
+        giphyService.addToFavourites(item);
+    }
 
-  main.getFavourites();
+    main.removeFromFavourites = function(item) {
+        giphyService.removeFromFavourites(item);
+    }
+
+    main.getFavourites = function() {
+        let results = giphyService.getFavourites().then(
+            function(results) {
+                main.favourites = results.data.favourites;
+            },
+            function(fail) {
+                console.log(fail);
+            });
+    }
+
+    main.getFavourites();
 }
